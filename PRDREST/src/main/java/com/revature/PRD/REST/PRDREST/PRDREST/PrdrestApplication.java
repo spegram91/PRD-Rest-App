1fe299326d05be4349e91219ba06fdeeaa3c2d92
spegram91 <spegram91@gmail.com>
2017-12-06 17:55:15 -0500

    first commit

---
+++ PRDREST/src/main/java/com/revature/PRD/REST/PRDREST/PRDREST/PrdrestApplication.java
@@ -0,0 +1,12 @@
+package com.revature.PRD.REST.PRDREST.PRDREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrdrestApplication.class, args);
+	}
+}
---
+++ PRDREST/src/test/java/com/revature/PRD/REST/PRDREST/PRDREST/PrdrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.PRD.REST.PRDREST.PRDREST;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrdrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
